f0d3e90cf332b6d80e8b710be728ecb48a896f4c
Engi Lozada <engilozada@gmail.com>
2023-03-16 01:09:49 +0100

    Create VideoDetail component and Final styles

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,17 @@
 import { Stack, Box } from "@mui/material";
 
-import { ChannelCard, VideoCard } from "./";
+import { ChannelCard, VideoCard, Loader } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
